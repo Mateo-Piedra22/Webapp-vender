7120979d3621dccad6960939c7d631878c96ee40
Mateo-Piedra22 <piedrabuena.mateo03@gmail.com>
2025-11-10 17:21:42 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public', 'index.html'),
-      },
-    },
-  },
 });
